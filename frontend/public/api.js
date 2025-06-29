260496312236a165f1a1c2b1891f751e8396c62f
gabca <g.crv@outlook.com>
2025-06-29 21:59:36 +0100

    deployment: gitignore for node_modules and frontend hooking

--- frontend/public/api.js
+++ frontend/public/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:3000/api';
+export const API_BASE = 'https://trabalho2-mashup-apis-gabfca-backend.onrender.com/api';
 
 export const api = {
     searchTrailer: async (movieName) => {
